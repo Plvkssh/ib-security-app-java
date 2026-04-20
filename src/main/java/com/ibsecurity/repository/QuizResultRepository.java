ea55dd91f60e015d0e8a103f9088e95d503a91f7
Plvkssh <kksmartregion@gmail.com>
2026-04-20 20:56:57 +0700

    Create QuizResultRepository.java

---
+++ src/main/java/com/ibsecurity/repository/QuizResultRepository.java
@@ -0,0 +1,12 @@
+package com.ibsecurity.repository;
+
+import com.ibsecurity.model.QuizResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuizResultRepository extends JpaRepository<QuizResult, Long> {
+    List<QuizResult> findByUser_IdOrderByCreatedAtDesc(Long userId);
+    Optional<QuizResult> findTopByUser_IdOrderByCreatedAtDesc(Long userId);
+}
