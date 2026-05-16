dc063554dc7bef8f11614b429de6168c67d9d57b
Plvkssh <kksmartregion@gmail.com>
2026-05-16 19:33:59 +0700

    Update GigaChatService.java

--- src/main/java/com/ibsecurity/service/GigaChatService.java
+++ src/main/java/com/ibsecurity/service/GigaChatService.java
@@ -17,8 +17,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
@@ -42,7 +40,7 @@ public class GigaChatService {
 
     public GigaChatService(RagRetrievalService ragRetrievalService) {
         this.ragRetrievalService = ragRetrievalService;
-        this.httpClient = createInsecureHttpClient();
+        this.httpClient = HttpClient.newHttpClient();
     }
 
     private String getAccessToken() throws Exception {
@@ -412,22 +410,6 @@ public class GigaChatService {
         }
         return text;
     }
-
-    private static HttpClient createInsecureHttpClient() {
-        try {
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return new X509Certificate[0];
-                        }
-
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        }
-
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        }
-                    }
-            };
 
             SSLContext sslContext = SSLContext.getInstance("TLS");
             sslContext.init(null, trustAllCerts, new SecureRandom());
